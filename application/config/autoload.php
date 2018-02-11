a505827b556eebc6f7ce1e7d311cc1aa7e258484
shafeeqthaliyan <36336341+shafeeqthaliyan@users.noreply.github.com>
2018-02-11 20:35:22 +0530

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('simple_html_dom');
+$autoload['libraries'] = array();
 
 /*
 | -------------------------------------------------------------------
